b4daa00a2eb9354deccf8f1bab8a47be9e2cb56f
raavimanikanta <raavi,ani123@gmail.com>
2022-03-12 22:34:18 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Home from "./components/Home"
+import Student from "./components/Student"
+import Contact from "./components/Contact"
+import Navbar from "./components/Navbar"
+import StudentProvider from "./components/Studentcontext"
+import Addstudent from "./components/Addstudent"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App =() => {
+    return(
+        <StudentProvider>
+        <div className="background">
+        
+        <Router>
+            <Navbar/>
+            <Routes>
+            <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/student" element={<Student />} />
+                <Route path="/Addstudent" element={<Addstudent />} />
+                 <Route path="/Addstudent/:id" element={<Addstudent/>}/> 
+                <Route path="/contact" element={<Contact/>} />
+            </Routes>
+            
+        </Router>
+        
+        
+             
+    
+        </div>
+        </StudentProvider>
+    )
+
 }
-
-export default App;
+export default App
